ae1349f69177af13b4af00746792eec00279a379
phi <nguyentruongphi15032003@gmail.com>
2022-08-27 19:13:51 +0700

    kiểu dữ liệu

---
+++ main.js
@@ -0,0 +1,43 @@
+// Number
+var a = 1
+
+console.log(typeof a)
+// String
+var fullName = 'PHI NGUYEN'
+
+// Boolean
+var isString = true
+
+// Underfind
+var age
+
+// Null
+var a = null // khong co gi
+
+// Symbol
+var id = Symbol('id')
+var id2 = Symbol('id')
+
+
+// Function
+var myFunction = function() {
+    alert('Hi. Xin chao cac ban!')
+}
+
+// Object types
+
+var myObject = {
+    name: 'PHI NGUYEN',
+    age: 20,
+    address: 'hcm',
+    myFunction: function() {
+
+    }
+}
+
+var myArray = [
+    'PHI NGUYEN',
+    'Javascript',
+    'Python'
+]
+
